f0f671916523fe0b53a12abc450792c4cf45656d
Tymofii-Oleksiuk <129986627+Tymofii-Oleksiuk@users.noreply.github.com>
2024-12-04 19:51:35 +0100

    fix

--- index.js
+++ index.js
@@ -2,17 +2,34 @@
 // const message = alert("Hello World");
 // console.log(displayMessage(message));
 
-const guessNumber = (number) => {
-  const message = prompt("Ведіть рандомне число");
-  if (message === number) {
-    return alert("Ви вгадали!!!!! :)");
-  } else {
-    return alert(`Неправильно: Було загадано ${number}`);
+// const guessNumber = (number) => {
+//   const message = prompt("Ведіть рандомне число");
+//   if (message === number) {
+//     return alert("Ви вгадали!!!!! :)");
+//   } else {
+//     return alert(`Неправильно: Було загадано ${number}`);
+//   }
+// };
+
+// let randomNumber = Math.round(Math.random() * (100 - 1) + 1);
+// console.log(guessNumber(randomNumber));
+
+const clickMessage = (clicks) => {
+  for (let i = 0; i < 20; i++) {
+    if (clicks === 10) {
+      alert("Ви достигли ліміту кліків");
+      break;
+    } else {
+      clicks = i;
+      console.log(clicks);
+      alert(`Ви зробили ${clicks} кліків`);
+      continue;
+    }
   }
 };
-
-let randomNumber = Math.round(Math.random() * (100 - 1) + 1);
-console.log(guessNumber(randomNumber));
+const message = alert("Клікніть на кнопочку");
+const totalClicks = 0;
+console.log(clickMessage(message, totalClicks));
 
 // const countSum = (a, b, c) => a + b + c;
 
